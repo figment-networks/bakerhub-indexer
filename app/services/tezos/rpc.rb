90fa7ff7ac4b8d640b50affe16596164645ca451
David Van Der Beek <earlynovrock@gmail.com>
2020-03-11 14:09:00 -0500

    let RPC accept query params

--- app/services/tezos/rpc.rb
+++ app/services/tezos/rpc.rb
@@ -6,21 +6,22 @@ module Tezos
       @chain = chain
     end
 
-    def self.get(path)
-      new.get(path)
+    def self.get(path, query = nil)
+      new.get(path, query)
     end
 
-    def url(path)
+    def url(path, query = nil)
       URI::Generic.build(
         scheme: chain.use_ssl_for_rpc? ? "https" : "http",
         host:   chain.rpc_host.presence || "localhost",
         port:   chain.rpc_port,
-        path:   [chain.rpc_path.sub(/\/$/, ''), "chains/#{chain.internal_name}", path].join("/")
+        path:   [chain.rpc_path.sub(/\/$/, ''), "chains/#{chain.internal_name}", path].join("/"),
+        query:  query
       ).to_s
     end
 
-    def get(path)
-      res = Typhoeus.get(url(path))
+    def get(path, query = nil)
+      res = Typhoeus.get(url(path, query))
       JSON.parse(res.body)
     end
   end
