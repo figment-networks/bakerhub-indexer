235d10574a175435c17540210054f13b6185225b
David Van Der Beek <earlynovrock@gmail.com>
2020-04-14 09:57:11 -0400

    alias attribute

--- app/models/tezos/event/missed_bake.rb
+++ app/models/tezos/event/missed_bake.rb
@@ -1,6 +1,7 @@
 module Tezos
   class Event
     class MissedBake < Tezos::Event
+      alias_attribute :baker, :sender
     end
   end
 end
--- app/models/tezos/event/missed_endorsement.rb
+++ app/models/tezos/event/missed_endorsement.rb
@@ -1,6 +1,7 @@
 module Tezos
   class Event
     class MissedEndorsement < Tezos::Event
+      alias_attribute :baker, :sender
     end
   end
 end
