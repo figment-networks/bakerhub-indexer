4229b07ba1c8ac09b96de882601353fd2ac9d798
Adam Van Der Beek <adam.vanderbeek@gmail.com>
2020-06-18 14:54:18 -0400

    Re-order

--- app/models/tezos/voting_period.rb
+++ app/models/tezos/voting_period.rb
@@ -2,9 +2,6 @@ class Tezos::VotingPeriod < ApplicationRecord
   belongs_to :chain
   has_many :ballots
 
-  alias quorum_reached quorum_reached?
-  alias supermajority_reached supermajority_reached?
-
   def quorum_reached?
     rolls_voted = self.ballots.sum(&:rolls)
     return false if self.quorum.blank?
@@ -18,6 +15,9 @@ class Tezos::VotingPeriod < ApplicationRecord
     total_rolls_voted = yay_rolls + self.ballots.where(vote: "nay").sum { |b| b.rolls }
     (yay_rolls.to_f / total_rolls_voted.to_f) >= 0.80
   end
+
+  alias quorum_reached quorum_reached?
+  alias supermajority_reached supermajority_reached?
 
   def end_time_approximation
     self.period_start_time + 22.days + 18.hours
