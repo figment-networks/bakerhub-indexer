b524225c531727e81f0445efe827d96e293cf68a
David Van Der Beek <earlynovrock@gmail.com>
2020-09-15 07:58:58 -0500

    get test working

--- spec/models/tezos/block_spec.rb
+++ spec/models/tezos/block_spec.rb
@@ -12,10 +12,19 @@ RSpec.describe Tezos::Block do
     it "falls back to calling endorsers sync service" do
       block = create(:tezos_block, endorsers: nil )
 
-      endorsers_array = ["asdf123", "testing123"]
-      callback = -> { block.update(endorsers: endorsers_array) }
-      allow_any_instance_of(Tezos::EndorsersSyncService).to receive(:run).and_return(endorsers_array)
-      allow_any_instance_of(Tezos::EndorsersSyncService).to receive(:on_success).and_return(callback)
+      sync = Tezos::EndorsersSyncService.new(block.chain, block.height)
+      url = sync.send(:url)
+
+      json = [
+        {
+          slots: [ 0, 1, 2, 3 ],
+          delegate: "test123"
+        }
+      ].to_json
+      endorsers_array = ["test123", "test123", "test123", "test123"]
+
+      response = Typhoeus::Response.new(code: 200, body: json)
+      Typhoeus.stub(url).and_return(response)
       
       expect(block.endorsers).to eq endorsers_array
     end
